acccf96891d2ff1caf319db7eb54357871b9e53c
pikamau5 <lartzu@gmail.com>
2019-09-14 11:38:00 +0300

    Added popups + log write stuff

--- maya_pyqt_template.py
+++ maya_pyqt_template.py
@@ -25,6 +25,7 @@ class MayaUiTemplate(QtWidgets.QWidget, Ui_Form):
         super(MayaUiTemplate, self).__init__(None)
         self._ui = Ui_Form()
         self._ui.setupUi(self)
+        self.log_path = "PATH_TO_LOG_FILE.txt"
 
     def setup_ui(self):
         """
@@ -35,6 +36,58 @@ class MayaUiTemplate(QtWidgets.QWidget, Ui_Form):
         super(MayaUiTemplate, self).setupUi(form)
 
         form.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)  # Window always on top
+
+    def message_query(self, title, message):
+        """
+        Popup to ask user input
+        Args:
+            title: Title of the popup
+            message: Message of the popup
+        Returns: True / False for the user's answer
+        """
+        result = QtWidgets.QMessageBox.question(self, title, message,
+                                                QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+                                                QtWidgets.QMessageBox.No)
+        if result == QtWidgets.QMessageBox.StandardButton.Yes:
+            answer = True
+        else:
+            answer = False
+        self.write_to_log(title + " | " + message + " | " + str(answer))
+        return answer
+
+    def handle_error(self, error_message):
+        """
+        Handle error by sending an error message
+        Args:
+            error_message: the error message
+        """
+        QtWidgets.QMessageBox.warning(self, "Warning", error_message)
+        print error_message
+        self.write_to_log(error_message)
+
+    def popup_message(self, title, message):
+        """
+        A popup message
+        Args:
+            title: Title of the popup message
+            message: Body of the popup message
+        """
+        QtWidgets.QMessageBox.information(self, title, message)
+        self.write_to_log(title + " | " + message)
+
+    def write_to_log(self, message):
+        """
+        Write message to log file
+        Args:
+            message: message to write to log
+        """
+        print message
+        try:
+            f = open(self.log_path, "a+")
+            f.write("\n" + message)
+        except Exception as e:
+            print str(e)
+            print "Couldn't append to log " + self.log_path
 
 
 uiTemplate = MayaUiTemplate()
